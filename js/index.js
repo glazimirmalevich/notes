32be28ca0a4375037ef856690ee4a812969f7b58
glazimirmalevich <torekhan.olzhas123@gmail.com>
2023-04-10 17:34:45 +0600

    Добавил textarea и поиск по тексту и тайтлу

--- js/index.js
+++ js/index.js
@@ -1,58 +1,178 @@
-const addRowInput = document.querySelector(".take_note");
+const noteText = document.querySelector(".take_note");
 const rows = document.querySelector(".notes__list");
 const search = document.querySelector(".header__searchbar");
 const pin = document.querySelector(".notes__pin");
+const addNote = document.querySelector(".notes__add");
+const noteTitle = document.querySelector(".take_note_title");
+const searchNote = document.querySelector(".search_note");
+const selectTextareas = document.querySelectorAll("textarea");
 let objectOfRows = {};
-let objectColors = {};
-function getRandomColor(){
-    const colors = ["#E8AEB7", "#B8E1FF", "#A9FFF7", "#94FBAB", "#82ABA1", "#DBF4A7", "#DFC2F2"];
-    let randomColors = Math.floor(Math.random() * colors.length);
-    return colors[randomColors];
+let objectOfColors = {};
+let objectOfTitles = {};
+let lastScrollHeight = 0;
+let heightCounter = 1;
+document.querySelector("button").addEventListener("click", ()=>{
+  localStorage.clear();
+  rows.innerHTML = '';
+})
+function expandArea(element){
+  element.addEventListener("input", ()=>{
+    if(lastScrollHeight !== element.scrollHeight){
+      element.setAttribute("rows", heightCounter++);
+    }
+    lastScrollHeight = element.scrollHeight;
+  });
 }
-function insertHTML(value, color){
-    rows.insertAdjacentHTML(
-        `afterbegin`,
-        `
+expandArea(noteText);
+expandArea(noteTitle);
+
+function getRandomColor() {
+  const colors = [
+    "#E8AEB7",
+    "#B8E1FF",
+    "#A9FFF7",
+    "#94FBAB",
+    "#82ABA1",
+    "#DBF4A7",
+    "#DFC2F2",
+  ];
+  let randomColors = Math.floor(Math.random() * colors.length);
+  return colors[randomColors];
+}
+function insertHTML(title, value, color) {
+  rows.insertAdjacentHTML(
+    `afterbegin`,
+    `
         <div class="notes__list__item" style="background-color: ${color}">
-        <div class="notes__list__item__title">Hello world!</div>
+        <div class="notes__list__item__title">${title}</div>
         <div class="notes__list__item__body">${value}</div>
     </div>   `
-      );
+  );
 }
-function insertRow(value){
-    objectOfRows = JSON.parse(localStorage.getItem("textRows"));
-    objectColors = JSON.parse(localStorage.getItem("usedColors"));
-    let usedColor = getRandomColor();
-    if (Object.keys(objectOfRows).length === 0) {
-        objectOfRows[`0`] = addRowInput.value;
-        objectColors[`0`] = usedColor;
-        console.log("creating value and usedColor if storage is 0", addRowInput.value, usedColor);
-    } else {
-        objectOfRows[`${Object.keys(objectOfRows).length}`] = addRowInput.value;
-        objectColors[`${Object.keys(objectColors).length}`] = usedColor;
-    }
-    insertHTML(value, usedColor);
-    addRowInput.value = "";
-    localStorage.setItem("usedColors", JSON.stringify(objectColors));
-    localStorage.setItem("textRows", JSON.stringify(objectOfRows));
+function insertRow(title, value) {
+  objectOfRows = JSON.parse(localStorage.getItem("textRows"));
+  objectOfColors = JSON.parse(localStorage.getItem("colorRows"));
+  objectOfTitles = JSON.parse(localStorage.getItem("titleRows"));
+  let usedColor = getRandomColor();
+  if (Object.keys(objectOfRows).length === 0) {
+    objectOfRows[`0`] = noteText.value;
+    objectOfColors[`0`] = usedColor;
+    objectOfTitles[`0`] = noteTitle.value;
+  } else {
+    objectOfRows[`${Object.keys(objectOfRows).length}`] = noteText.value;
+    objectOfColors[`${Object.keys(objectOfColors).length}`] = usedColor;
+    objectOfTitles[`${Object.keys(objectOfTitles).length}`] = noteTitle.value;
+  }
+  insertHTML(title, value, usedColor);
+  noteText.value = "";
+  noteTitle.value = "";
+  localStorage.setItem("colorRows", JSON.stringify(objectOfColors));
+  localStorage.setItem("textRows", JSON.stringify(objectOfRows));
+  localStorage.setItem("titleRows", JSON.stringify(objectOfTitles));
 }
-if (localStorage.getItem("textRows") && localStorage.getItem("usedColors")) {
+if (
+  localStorage.getItem("textRows") &&
+  localStorage.getItem("colorRows") &&
+  localStorage.getItem("titleRows")
+) {
   let storageRows = JSON.parse(localStorage.getItem("textRows"));
-  let storageColors = JSON.parse(localStorage.getItem("usedColors"));
+  let storageColors = JSON.parse(localStorage.getItem("colorRows"));
+  let storageTitles = JSON.parse(localStorage.getItem("titleRows"));
   for (let i = 0; i < Object.keys(storageRows).length; i++) {
-    insertHTML(storageRows[i], storageColors[i]);
+    insertHTML(storageTitles[i], storageRows[i], storageColors[i]);
   }
 }
-addRowInput.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    if (addRowInput.value !== "" && addRowInput.value.length > 0) {
+searchNote.addEventListener("input", (event) => {
+    let storageRows = JSON.parse(localStorage.getItem("textRows"));
+    let storageColors = JSON.parse(localStorage.getItem("colorRows"));
+    let storageTitles = JSON.parse(localStorage.getItem("titleRows"));
+    rows.innerHTML = "";
+    if (event.target.value !== "" && event.target.value !== undefined) {
+        for (let i = 0; i < Object.keys(storageTitles).length; i++) {
+          if (storageTitles[i].includes(event.target.value) || storageRows[i].includes(event.target.value)) {
+            rows.insertAdjacentHTML(
+              `afterbegin`,
+              `
+                <div class="notes__list__item" style="background-color: ${storageColors[i]}">
+                <div class="notes__list__item__title">${storageTitles[i]}</div>
+                <div class="notes__list__item__body">${storageRows[i]}</div>
+            </div>   `
+            );
+          } else {
+            console.log("Not includes: ", storageTitles[i]);
+          }
+        }
+
+    } else {
+      for (let i = 0; i < Object.keys(storageTitles).length; i++) {
+          rows.insertAdjacentHTML(
+            `afterbegin`,
+            `
+              <div class="notes__list__item" style="background-color: ${storageColors[i]}">
+              <div class="notes__list__item__title">${storageTitles[i]}</div>
+              <div class="notes__list__item__body">${storageRows[i]}</div>
+          </div>   `
+          );
+      }
+    }
+  //   rows.innerHTML =     `
+  //   <div class="notes__list__item" style="background-color: orange">
+  //   <div class="notes__list__item__title">${event.target.value}</div>
+  //   <div class="notes__list__item__body">${event.target.value}</div>
+  // </div>   `  ;
+});
+document.body.addEventListener("click", (event) => {
+  if (addNote.contains(event.target)) {
+    if (addNote.childNodes.length < 5) {
+      noteTitle.classList.remove("take_note_title");
+      noteTitle.classList.add("take_note_title_active");
+    }
+  } else {
+    noteTitle.classList.remove("take_note_title_active");
+    noteTitle.classList.add("take_note_title");
+    if (
+      noteText.value !== "" &&
+      noteText.value.length > 0 &&
+      noteTitle.value !== "" &&
+      noteText.value.length > 0
+    ) {
       if (localStorage.getItem("textRows") === null) {
         localStorage.setItem("textRows", JSON.stringify({}));
-        localStorage.setItem("usedColors", JSON.stringify({}));
-        insertRow(addRowInput.value);
+        localStorage.setItem("colorRows", JSON.stringify({}));
+        localStorage.setItem("titleRows", JSON.stringify({}));
+        insertRow(noteTitle.value, noteText.value);
       } else {
-        insertRow(addRowInput.value);
+        insertRow(noteTitle.value, noteText.value);
       }
+    }
+    noteTitle.value = "";
+    noteText.value = "";
+    noteText.setAttribute("rows", 1);
+    noteTitle.setAttribute("rows", 1);
+    lastScrollHeight = 0;
+    heightCounter = 1;
+  }
+});
+addNote.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    if (
+      noteText.value !== "" &&
+      noteText.value.length > 0 &&
+      noteTitle.value !== "" &&
+      noteText.value.length > 0
+    ) {
+      if (localStorage.getItem("textRows") === null) {
+        localStorage.setItem("textRows", JSON.stringify({}));
+        localStorage.setItem("colorRows", JSON.stringify({}));
+        localStorage.setItem("titleRows", JSON.stringify({}));
+        insertRow(noteTitle.value, noteText.value);
+      } else {
+        insertRow(noteTitle.value, noteText.value);
+      }
+      noteText.setAttribute("rows", 1);
+      noteTitle.setAttribute("rows", 1);
+      lastScrollHeight = 0;
+      heightCounter = 1;
     }
   }
 });
